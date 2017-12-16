00fea095e26b4ae8bf2b725423552a5076b113ed
sleung852 <sleung852@gmail.com>
2017-12-17 00:27:45 +0800

    Commit message

--- 6.0002 Introduction to Computational Thinking and Data Science/Lecture2/lecture 2 - run.py
+++ 6.0002 Introduction to Computational Thinking and Data Science/Lecture2/lecture 2 - run.py
@@ -60,7 +60,9 @@ def maxVal(toConsider, avail):
     """Assumes toConsider a list of items, avail a weight
        Returns a tuple of the total value of a solution to the
          0/1 knapsack problem and the items of that solution"""
+#avail -> available weight
     if toConsider == [] or avail == 0:
+        #records the best solution found so far
         result = (0, ())
     elif toConsider[0].getCost() > avail:
         #Explore right branch only
@@ -99,6 +101,9 @@ testGreedys(foods, 750)
 print('')
 testMaxVal(foods, 750)
 
+#since the number of items is so small, any algro would work
+
+#now try a larger menu
 import random
 
 def buildLargeMenu(numItems, maxVal, maxCost):
@@ -109,76 +114,7 @@ def buildLargeMenu(numItems, maxVal, maxCost):
                           random.randint(1, maxCost)))
     return items
 
-#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
-#    print('Try a menu with', numItems, 'items')
-#    items = buildLargeMenu(numItems, 90, 250)
-#    testMaxVal(items, 750, False)  
-
-def fib(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
-
-#for i in range(121):
-#    print('fib(' + str(i) + ') =', fib(i))
-
-
-def fastFib(n, memo = {}):
-    """Assumes n is an int >= 0, memo used only by recursive calls
-       Returns Fibonacci of n"""
-    if n == 0 or n == 1:
-        return 1
-    try:
-        return memo[n]
-    except KeyError:
-        result = fastFib(n-1, memo) + fastFib(n-2, memo)
-        memo[n] = result
-        return result
-
-#for i in range(121):
-#    print('fib(' + str(i) + ') =', fastFib(i))
-
-def fastMaxVal(toConsider, avail, memo = {}):
-    """Assumes toConsider a list of subjects, avail a weight
-         memo supplied by recursive calls
-       Returns a tuple of the total value of a solution to the
-         0/1 knapsack problem and the subjects of that solution"""
-    if (len(toConsider), avail) in memo:
-        result = memo[(len(toConsider), avail)]
-    elif toConsider == [] or avail == 0:
-        result = (0, ())
-    elif toConsider[0].getCost() > avail:
-        #Explore right branch only
-        result = fastMaxVal(toConsider[1:], avail, memo)
-    else:
-        nextItem = toConsider[0]
-        #Explore left branch
-        withVal, withToTake =\
-                 fastMaxVal(toConsider[1:],
-                            avail - nextItem.getCost(), memo)
-        withVal += nextItem.getValue()
-        #Explore right branch
-        withoutVal, withoutToTake = fastMaxVal(toConsider[1:],
-                                                avail, memo)
-        #Choose better branch
-        if withVal > withoutVal:
-            result = (withVal, withToTake + (nextItem,))
-        else:
-            result = (withoutVal, withoutToTake)
-    memo[(len(toConsider), avail)] = result
-    return result
-
-def testMaxVal(foods, maxUnits, algorithm, printItems = True):
-    print('Menu contains', len(foods), 'items')
-    print('Use search tree to allocate', maxUnits,
-          'calories')
-    val, taken = algorithm(foods, maxUnits)
-    if printItems:
-        print('Total value of items taken =', val)
-        for item in taken:
-            print('   ', item)
-          
-#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45, 50):
-#    items = buildLargeMenu(numItems, 90, 250)
-#    testMaxVal(items, 750, fastMaxVal, True)
+for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
+    print('Try a menu with', numItems, 'items')
+    items = buildLargeMenu(numItems, 90, 250)
+    testMaxVal(items, 750, False)  
